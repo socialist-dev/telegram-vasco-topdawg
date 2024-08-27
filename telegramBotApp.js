7150c5c895148b9cac404d24f2378636c0187ee6
hoanglong <87950841+socialist-dev@users.noreply.github.com>
2024-08-28 02:34:40 +0700

    Add files via upload

--- telegramBotApp.js
+++ telegramBotApp.js
@@ -1,16 +1,15 @@
 // Gọi phương thức expand để mở rộng bot app ra toàn màn hình
 document.addEventListener("DOMContentLoaded", function() {
     Telegram.WebApp.expand();
-    Telegram.WebApp.disableVerticalSwipes();
 });
 
-// function initializeApp() {
-//     // Tự động tắt vuốt dọc khi khởi động
-//     Telegram.WebApp.disableVerticalSwipes();
+function initializeApp() {
+    // Tự động tắt vuốt dọc khi khởi động
+    Telegram.WebApp.disableVerticalSwipes();
     
-//     // Các thao tác khởi tạo khác
-// }
+    // Các thao tác khởi tạo khác
+}
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     initializeApp();
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    initializeApp();
+});
