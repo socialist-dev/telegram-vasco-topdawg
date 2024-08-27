8b13c54fdadfb72bfbc5db23e6248b2f9dcb6255
hoanglong <87950841+socialist-dev@users.noreply.github.com>
2024-08-28 03:13:02 +0700

    Add files via upload

--- telegramBotApp.js
+++ telegramBotApp.js
@@ -6,5 +6,5 @@ document.addEventListener("DOMContentLoaded", function() {
     Telegram.WebApp.disableVerticalSwipes();
 
     // Thay đổi màu của phần đầu (header)
-    Telegram.WebApp.themeParams.headerColor = "#FF5733"; // Sử dụng giá trị phù hợp cho màu sắc
+    Telegram.WebApp.setHeaderColor('#FF5733'); // Sử dụng giá trị phù hợp cho màu sắc
 });
